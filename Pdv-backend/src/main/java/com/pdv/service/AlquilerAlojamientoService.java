f951a351740949255a9c114ba37ac3666ad80706
David <ddorluc760@iesalmudeyne.es>
2024-04-11 17:03:52 +0200

    Creación de servicios para cada clase

---
+++ Pdv-backend/src/main/java/com/pdv/service/AlquilerAlojamientoService.java
@@ -0,0 +1,8 @@
+package com.pdv.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AlquilerAlojamientoService {
+
+}
---
+++ Pdv-backend/src/main/java/com/pdv/service/ImagenAlojamientoService.java
@@ -0,0 +1,5 @@
+package com.pdv.service;
+
+public class ImagenAlojamientoService {
+
+}
