64ae90b118cf2f6f33c934523b282a964137c901
David <ddorluc760@iesalmudeyne.es>
2024-04-11 17:19:26 +0200

    Creación de los controladores de cada servicio

---
+++ Pdv-backend/src/main/java/com/pdv/controller/AlquilerAlojamientoController.java
@@ -0,0 +1,10 @@
+package com.pdv.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/alquilerAlojamiento")
+public class AlquilerAlojamientoController {
+
+}
