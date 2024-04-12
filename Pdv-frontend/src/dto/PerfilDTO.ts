b0ef77c5495a415522097da858d3e36f56d433ec
jmarcan286 <jmarcan286@iesalmudeyne.es>
2024-04-12 16:02:31 +0200

    Creacion de PerfilDTO y UsuarioDTO

--- Pdv-frontend/src/dto/PerfilDTO.ts
+++ Pdv-frontend/src/dto/PerfilDTO.ts
@@ -1,3 +1,36 @@
-export class perfilDTO{
-    
+import { UsuarioDTO } from "./UsuarioDTO";
+
+export class PerfilDTO{
+
+    private id?: number;
+    private codPerfil?: String;
+    private idUsuario?: UsuarioDTO;
+
+    constructor(){
+
+    }
+
+    set setId(id:number){
+        this.id = id;
+    }
+
+    get getId(){
+        return this.id;
+    }
+
+    set setCodPerfil(codPerfil: String){
+        this.codPerfil = codPerfil;
+    }
+
+    get getCodPerfil(){
+        return this.codPerfil;
+    }
+
+    set setIdUsuario(usuario: UsuarioDTO){
+        this.idUsuario = usuario;
+    }
+
+    get getIdUsuario(){
+        return this.idUsuario;
+    }
 }
---
+++ Pdv-frontend/src/dto/UsuarioDTO.ts
@@ -0,0 +1,18 @@
+import { PerfilDTO } from "./PerfilDTO";
+
+export class UsuarioDTO{
+
+    private id?: Number;
+
+    private txtDescripcion?: String;
+
+    private txtDni?: String;
+
+    private numTelefono?: Number;
+
+    private txtEmail?: String;
+
+    private txtPassword?: String;
+
+    private idPerfil?: PerfilDTO;
+}
