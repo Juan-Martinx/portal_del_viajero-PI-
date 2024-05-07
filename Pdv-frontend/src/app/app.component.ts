1135ebf796a82672949674ba7f8c603e30fe4bf4
David <ddorluc760@iesalmudeyne.es>
2024-05-07 15:54:25 +0200

    Creación del componente de la página de registro y empezando con el diseño de la página

--- Pdv-frontend/src/app/app.component.ts
+++ Pdv-frontend/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PaginaRegistroComponent } from './paginas/pagina-registro/pagina-registro.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, PaginaRegistroComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ Pdv-frontend/src/app/paginas/pagina-registro/pagina-registro.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {FormsModule} from '@angular/forms';
+import { FormGroupDirective, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
+
+@Component({
+  selector: 'app-pagina-registro',
+  standalone: true,
+  imports: [FormsModule, MatFormFieldModule, MatInputModule, ReactiveFormsModule],
+  templateUrl: './pagina-registro.component.html',
+  styleUrl: './pagina-registro.component.css'
+})
+export class PaginaRegistroComponent {
+
+  usuarios = new FormGroup({
+    usuario: new FormControl('', Validators.required),
+    contrasena: new FormControl('', [Validators.required, Validators.minLength(6), Validators.maxLength(12)]),
+    dni: new FormControl('', [Validators.required, Validators.pattern('^[0-9]{8}[A-Z]')]),
+    telefono: new FormControl('', [Validators.required, Validators.pattern('^[0-9]{9}')]),
+    correoElectronico: new FormControl('', [Validators.required, Validators.email])
+  })
+
+}
