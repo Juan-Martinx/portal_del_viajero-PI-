6d5746e92d4dfecca129a09feb3a2e7c46d5ad51
David <ddorluc760@iesalmudeyne.es>
2024-05-22 16:11:36 +0200

    Pequeño arreglo en el frontend

--- Pdv-frontend/src/app/app.component.ts
+++ Pdv-frontend/src/app/app.component.ts
@@ -1,19 +1,5 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { RouterOutlet, Router, NavigationEnd } from '@angular/router';
-import { PaginaRegistroComponent } from './paginas/pagina-registro/pagina-registro.component';
-import { InicioSesionComponent } from './paginas/inicio-sesion/inicio-sesion.component';
-import { ConvertirGestorComponent } from './paginas/convertir-gestor/convertir-gestor.component';
-import { EditarPerfilComponent } from './paginas/editar-perfil/editar-perfil.component';
-import { VerPerfilComponent } from './paginas/ver-perfil/ver-perfil.component';
-import { BuscarComodidadComponent } from './paginas/buscar-comodidad/buscar-comodidad.component';
-import { ModificarComodidadComponent } from './paginas/modificar-comodidad/modificar-comodidad.component';
-import { AnadirComodidadComponent } from './paginas/anadir-comodidad/anadir-comodidad.component';
-import { CasasAlquilerComponent } from './paginas/casas-alquiler/casas-alquiler.component';
-import { CasasDisponiblesComponent } from './paginas/casas-disponibles/casas-disponibles.component';
-import { VerReservasComponent } from './paginas/ver-reservas/ver-reservas.component';
-import { BuscarUsuariosComponent } from './paginas/buscar-usuarios/buscar-usuarios.component';
-import { DetallesCasaRuralGestorAdministradorComponent } from './paginas/detalles-casa-rural-gestor-administrador/detalles-casa-rural-gestor-administrador.component';
-import { DetallesCasaRuralClienteComponent } from './paginas/detalles-casa-rural-cliente/detalles-casa-rural-cliente.component';
 import { MenuComponent } from './components/menu/menu.component';
 import { HTTP_INTERCEPTORS, HttpClient, HttpClientModule } from '@angular/common/http';
 import { filter } from 'rxjs/operators';
@@ -21,10 +7,7 @@ import { filter } from 'rxjs/operators';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, PaginaRegistroComponent, InicioSesionComponent, ConvertirGestorComponent, EditarPerfilComponent, 
-    VerPerfilComponent, BuscarComodidadComponent, ModificarComodidadComponent, AnadirComodidadComponent, CasasAlquilerComponent, 
-    CasasDisponiblesComponent, VerReservasComponent, BuscarUsuariosComponent, DetallesCasaRuralGestorAdministradorComponent,
-    DetallesCasaRuralClienteComponent, MenuComponent, HttpClientModule],
+  imports: [RouterOutlet, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- Pdv-frontend/src/app/app.routes.ts
+++ Pdv-frontend/src/app/app.routes.ts
@@ -3,9 +3,10 @@ import { AuthorizedComponent } from './paginas/authorized/authorized.component';
 import { LogoutComponent } from './components/logout/logout.component';
 import { PaginaRegistroComponent } from './paginas/pagina-registro/pagina-registro.component';
 import { InicioSesionComponent } from './paginas/inicio-sesion/inicio-sesion.component';
+import { MenuComponent } from './components/menu/menu.component';
 
 export const routes: Routes = [
-    { path: '', component: PaginaRegistroComponent, title: 'Inicio'},
+    { path: '', component: MenuComponent, title: 'Inicio'},
     { path: 'registro', component: PaginaRegistroComponent, title: 'Inicio'},
     { path: 'inicio-sesion', component: InicioSesionComponent, title: 'Inicio'},
     { path: 'authorized', component: AuthorizedComponent },
