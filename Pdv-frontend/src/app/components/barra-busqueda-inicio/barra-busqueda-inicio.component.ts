449845f6a4546778bbee4e87850d716847d5f9db
David <ddorluc760@iesalmudeyne.es>
2024-05-23 18:12:20 +0200

    barra de busqueda terminada en cuanto al diseño

--- Pdv-frontend/src/app/components/barra-busqueda-inicio/barra-busqueda-inicio.component.ts
+++ Pdv-frontend/src/app/components/barra-busqueda-inicio/barra-busqueda-inicio.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormsModule, FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-barra-busqueda-inicio',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, FormsModule],
   templateUrl: './barra-busqueda-inicio.component.html',
   styleUrl: './barra-busqueda-inicio.component.css'
 })
 export class BarraBusquedaInicioComponent {
+
+  busqueda = new FormGroup({
+    destino: new FormControl(),
+    llegada: new FormControl(),
+    salida: new FormControl()
+  });
 
 }
