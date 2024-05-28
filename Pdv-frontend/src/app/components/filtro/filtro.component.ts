603726f00a10ede17f0f2345273c4b1f17711142
David <ddorluc760@iesalmudeyne.es>
2024-05-28 19:52:06 +0200

    Comienzo con el diseño del filtro a falta de terminarlo

--- Pdv-frontend/src/app/components/filtro/filtro.component.ts
+++ Pdv-frontend/src/app/components/filtro/filtro.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import {MatSliderModule} from '@angular/material/slider';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {FormsModule} from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import {provideNativeDateAdapter} from '@angular/material/core';
 
 @Component({
   selector: 'app-filtro',
   standalone: true,
-  imports: [],
+  providers: [provideNativeDateAdapter()],
+  imports: [MatSliderModule, MatFormFieldModule, MatInputModule, FormsModule, MatButtonModule, MatIconModule, MatDatepickerModule],
   templateUrl: './filtro.component.html',
   styleUrl: './filtro.component.css'
 })
 export class FiltroComponent {
 
+  formatLabel(value: number): string {
+   /* if (value >= 1000) {
+      return Math.round(value / 1000) + 'k';
+    }*/
+
+    return `${value}€`;
+  }
 }
